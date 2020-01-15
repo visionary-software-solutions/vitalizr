bfa7495c3dab33f5e2dc8b4d57eeecf10f3381b3
V <nick@visionary.software>
2020-01-15 03:09:56 -0800

    GOT IT TO WORK. Holy shit Socket/Stream lifecycles are confusing

--- src/main/java/software/visionary/vitalizr/ListWeightsForPerson.java
+++ src/main/java/software/visionary/vitalizr/ListWeightsForPerson.java
@@ -13,42 +13,16 @@ import java.util.stream.Collectors;
 public class ListWeightsForPerson implements BiConsumer<InputStream, OutputStream> {
     @Override
     public void accept(final InputStream received, final OutputStream sent) {
-        try {
+        try (final Scanner scanner = new Scanner(received, StandardCharsets.UTF_8.name());
+             final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(sent)))  {
             Vitalizr.loadAll();
-        } catch (IOException e) {
-            writeToOutput(e.getMessage(), sent);
-            closeConnection(sent);
-            return;
-        }
-        final String input = getInput(received);
-        final Person person = Human.createPerson(input);
-        final Collection<Weight> weights = Vitalizr.getWeightsFor(person);
-        writeToOutput(weights.stream().map(Object::toString).collect(Collectors.joining(",")), sent);
-        closeConnection(sent);
-    }
-
-    private void writeToOutput(final String message, final OutputStream sent) {
-        try (final BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(sent))){
-            writer.write(message);
+            final String input = scanner.next();
+            final Person person = Human.createPerson(input);
+            final Collection<Weight> weights = Vitalizr.getWeightsFor(person);
+            writer.write(weights.stream().map(Object::toString).collect(Collectors.joining(",")));
             writer.newLine();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private void closeConnection(final OutputStream sent) {
-        try {
-            sent.close();
-        } catch (IOException e) {
-            writeToOutput(e.getMessage(), sent);
-        }
-    }
-
-    private static String getInput(final InputStream received) {
-        String text;
-        try (Scanner scanner = new Scanner(received, StandardCharsets.UTF_8.name())) {
-            text = scanner.next();
-        }
-        return text;
     }
 }
