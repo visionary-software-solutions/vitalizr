bfd257c0e38b6ebb5f35ae546d232e5a5e2441d5
V <nick@visionary.software>
2020-01-17 18:29:24 -0800

    partition saving/loading blood sugars from file into their own feature tests.

--- src/test/java/software/visionary/vitalizr/features/LoadBloodSugarsFromFileIntegrationTest.java
+++ src/test/java/software/visionary/vitalizr/features/LoadBloodSugarsFromFileIntegrationTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import software.visionary.vitalizr.Fixtures;
 import software.visionary.vitalizr.Vitalizr;
 import software.visionary.vitalizr.api.Person;
-import software.visionary.vitalizr.bloodPressure.BloodPressure;
-import software.visionary.vitalizr.bloodPressure.Combined;
-import software.visionary.vitalizr.bloodPressure.CombinedBloodPressureConverter;
-import software.visionary.vitalizr.bloodPressure.CombinedBloodPressureSerializationProxy;
+import software.visionary.vitalizr.bloodSugar.BloodSugar;
+import software.visionary.vitalizr.bloodSugar.WholeBloodGlucose;
+import software.visionary.vitalizr.bloodSugar.WholeBloodGlucoseConverter;
+import software.visionary.vitalizr.bloodSugar.WholeBloodGlucoseSerializationProxy;
 import software.visionary.vitalizr.serialization.WriteObjectAsGZip;
 
 import java.io.File;
@@ -20,23 +20,23 @@ import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class LoadBloodPressuresFromFileIntegrationTest {
+class LoadBloodSugarsFromFileIntegrationTest {
     @Test
-    void canLoadBloodPressuresFromFile() throws IOException {
+    void canLoadBloodSugarsFromFile() throws IOException {
         // Given: A file containing some vitals for a person
         final Person mom = Fixtures.createRandomPerson();
-        final Combined toStore3 = Combined.systolicAndDiastolicBloodPressure(Instant.now().plus(-2, ChronoUnit.DAYS), 138, 89, mom);
+        final BloodSugar toStore4 = new WholeBloodGlucose(Instant.now().plus(-2, ChronoUnit.DAYS), 225, mom);
         final File data = Files.createFile(Paths.get(System.getProperty("user.dir"), mom.getEmailAddress().toString() + "_load_vitals")).toFile();
         data.deleteOnExit();
-        final CombinedBloodPressureSerializationProxy serialized3 = CombinedBloodPressureConverter.INSTANCE.to(toStore3);
-        final WriteObjectAsGZip<CombinedBloodPressureSerializationProxy> writer3 = new WriteObjectAsGZip<>(serialized3, data.toPath());
-        writer3.run();
+        final WholeBloodGlucoseSerializationProxy serialized4 = WholeBloodGlucoseConverter.INSTANCE.to((WholeBloodGlucose) toStore4);
+        final WriteObjectAsGZip<WholeBloodGlucoseSerializationProxy> writer4 = new WriteObjectAsGZip<>(serialized4, data.toPath());
+        writer4.run();
         // When: I call loadVitalsFromFile
         Vitalizr.loadVitalsFromFile(data);
         // And: I query for vitals I know are in that file
         // Then: The vitals should be returned
-        final Collection<BloodPressure> storedAsWell = Vitalizr.getBloodPressuresFor(mom);
-        assertTrue(storedAsWell.contains(toStore3));
+        final Collection<BloodSugar> storedToo = Vitalizr.getBloodSugarsFor(mom);
+        assertTrue(storedToo.contains(toStore4));
         data.delete();
     }
 }
---
+++ src/test/java/software/visionary/vitalizr/features/SaveBloodSugarsToFileIntegrationTest.java
@@ -0,0 +1,54 @@
+package software.visionary.vitalizr.features;
+
+import org.junit.jupiter.api.Test;
+import software.visionary.vitalizr.Fixtures;
+import software.visionary.vitalizr.Vitalizr;
+import software.visionary.vitalizr.api.Person;
+import software.visionary.vitalizr.bloodSugar.BloodSugar;
+import software.visionary.vitalizr.bloodSugar.StringWholeBloodGlucoseConverter;
+import software.visionary.vitalizr.bloodSugar.WholeBloodGlucose;
+import software.visionary.vitalizr.serialization.GZipFiles;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SaveBloodSugarsToFileIntegrationTest {
+    @Test
+    void canSaveVitalsToFile() throws IOException {
+        // Given: A person
+        final Person mom = Fixtures.createRandomPerson();
+        // And: Some Vitals for the person
+        final Collection<BloodSugar> fourthStored = bloodSugars(mom);
+        // And: Vitalizr has stored those vitals
+        fourthStored.forEach(Vitalizr::storeBloodSugarFor);
+        // And: A File to write the data to
+        final File data = Files.createFile(Paths.get(System.getProperty("user.dir"), mom.getEmailAddress().toString() + "_save_vitals")).toFile();
+        data.deleteOnExit();
+        // When: I call saveVitalsToFile
+        Vitalizr.saveVitalsToFile(data);
+        // Then: The vitals should be stored in the file
+        final List<String> written = GZipFiles.slurpGZippedFile(data.toPath(), StandardCharsets.UTF_8);
+        final List<BloodSugar> foundBloodSugars = StringWholeBloodGlucoseConverter.INSTANCE.to(written.stream()).collect(Collectors.toList());
+        assertTrue(foundBloodSugars.containsAll(fourthStored));
+        data.delete();
+    }
+
+    private static Collection<BloodSugar> bloodSugars(final Person person) {
+        final Collection<BloodSugar> alsoStored = new ArrayList<>(3);
+        alsoStored.add(new WholeBloodGlucose(Instant.now(), 240, person));
+        alsoStored.add(new WholeBloodGlucose(Instant.now().plus(-1, ChronoUnit.DAYS), 137, person));
+        alsoStored.add(new WholeBloodGlucose(Instant.now().plus(-2, ChronoUnit.DAYS), 199, person));
+        return alsoStored;
+    }
+}
