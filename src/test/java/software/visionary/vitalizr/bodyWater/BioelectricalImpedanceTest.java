6998739e9010e29acd7170405bb201cebebff8fa
V <nick@visionary.software>
2020-01-23 03:38:42 -0800

    more unit tests.

---
+++ src/test/java/software/visionary/vitalizr/bodyWater/BioelectricalImpedanceTest.java
@@ -0,0 +1,77 @@
+package software.visionary.vitalizr.bodyWater;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import software.visionary.vitalizr.Fixtures;
+import software.visionary.vitalizr.api.Lifeform;
+import software.visionary.vitalizr.api.Unit;
+
+import java.time.Instant;
+import java.util.concurrent.ThreadLocalRandom;
+
+class BioelectricalImpedanceTest {
+    private Lifeform lifeform;
+    private Instant observedAt;
+    private Number value;
+    private BioelectricalImpedance toTest;
+
+    @BeforeEach
+    void setup() {
+        lifeform = Fixtures.person();
+        observedAt = Instant.now();
+        value = ThreadLocalRandom.current().nextInt(0, 300);
+        toTest = new BioelectricalImpedance(observedAt, value, lifeform);
+    }
+
+    @Test
+    void rejectsNullInstant() {
+        Assertions.assertThrows(NullPointerException.class, () -> new BioelectricalImpedance(null, value,  lifeform));
+    }
+
+    @Test
+    void rejectsNullValue() {
+        Assertions.assertThrows(NullPointerException.class, () -> new BioelectricalImpedance(observedAt, null, lifeform));
+    }
+
+    @Test
+    void rejectsNullOwner() {
+        Assertions.assertThrows(NullPointerException.class, () -> new BioelectricalImpedance(observedAt, value, null));
+    }
+
+    @Test
+    void canGetInstant() {
+        Assertions.assertEquals(observedAt, toTest.observedAt());
+    }
+
+    @Test
+    void canGetQuantity() {
+        Assertions.assertEquals(value, toTest.getQuantity());
+    }
+
+    @Test
+    void defaultsToNoUnit() {
+        Assertions.assertEquals(Unit.NONE.INSTANCE, toTest.getUnit());
+    }
+
+    @Test
+    void implementsHashCodeCorrectly() {
+        Assertions.assertEquals(toTest.hashCode(), toTest.hashCode());
+        final BioelectricalImpedance another = new BioelectricalImpedance(observedAt, value, lifeform);
+        Assertions.assertEquals(toTest.hashCode(), another.hashCode());
+        Assertions.assertEquals(another.hashCode(), toTest.hashCode());
+    }
+
+    @Test
+    void implementsEqualsCorrectly() {
+        Assertions.assertNotEquals(toTest, null);
+        Assertions.assertNotEquals(toTest, new Object());
+        Assertions.assertEquals(toTest, toTest);
+        final BioelectricalImpedance another = new BioelectricalImpedance(observedAt, value, lifeform);
+        Assertions.assertEquals(toTest, another);
+        Assertions.assertEquals(another, toTest);
+        final BioelectricalImpedance aThird = new BioelectricalImpedance(observedAt, value, lifeform);
+        Assertions.assertEquals(another, aThird);
+        Assertions.assertEquals(toTest, aThird);
+    }
+}
