ab3fb3907cd94dd0714422881d718d3daee1f15d
V <nick@visionary.software>
2020-02-03 03:32:32 -0800

    lookup blood oxygen by ID

--- src/main/java/software/visionary/vitalizr/oxygen/ListBloodOxygens.java
+++ src/main/java/software/visionary/vitalizr/oxygen/ListBloodOxygens.java
@@ -1,18 +1,14 @@
 package software.visionary.vitalizr.oxygen;
 
-import software.visionary.vitalizr.Human;
-import software.visionary.vitalizr.ListVitals;
+import software.visionary.vitalizr.GetAllByID;
 import software.visionary.vitalizr.Vitalizr;
-import software.visionary.vitalizr.api.Person;
 
 import java.util.Collection;
-import java.util.Scanner;
+import java.util.UUID;
 
-public final class ListBloodOxygens extends ListVitals {
+public final class ListBloodOxygens extends GetAllByID {
     @Override
-    protected Collection<BloodOxygen> getVitals(final Scanner scanner) {
-        final String input = scanner.useDelimiter("\u0004").next();
-        final Person person = Human.createPerson(input);
-        return Vitalizr.getBloodOxygensFor(person);
+    protected Collection<BloodOxygen> forId(final UUID id) {
+        return Vitalizr.lookup(id, Vitalizr::getBloodOxygensFor);
     }
 }
--- src/test/java/software/visionary/vitalizr/oxygen/ListBloodOxygensTest.java
+++ src/test/java/software/visionary/vitalizr/oxygen/ListBloodOxygensTest.java
@@ -6,12 +6,8 @@ import software.visionary.vitalizr.Fixtures;
 import software.visionary.vitalizr.Vitalizr;
 import software.visionary.vitalizr.api.Person;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Collection;
-import java.util.Scanner;
 
 final class ListBloodOxygensTest {
     @Test
@@ -20,13 +16,9 @@ final class ListBloodOxygensTest {
         final Person p = Fixtures.createRandomPerson();
         final BloodOxygen saved = new PeripheralOxygenSaturation(Instant.now(), spO2, p);
         Vitalizr.storeBloodOxygen(saved);
-        final String input = String.format("%s\u0004", p);
-        final InputStream stream = new ByteArrayInputStream(input.getBytes(StandardCharsets.UTF_8));
-        final Scanner scanner = new Scanner(stream);
         final ListBloodOxygens action = new ListBloodOxygens();
-        final Collection<BloodOxygen> stored = action.getVitals(scanner);
+        final Collection<BloodOxygen> stored = action.forId(p.getID());
         Assertions.assertFalse(stored.isEmpty());
-        Assertions.assertTrue(stored.parallelStream()
-                .anyMatch(bp -> bp.getQuantity().equals(spO2)));
+        Assertions.assertTrue(stored.contains(saved));
     }
 }
