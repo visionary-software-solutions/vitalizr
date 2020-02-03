18753a9c33ae59d69f9dcd464a885bfad0263821
V <nick@visionary.software>
2020-02-03 03:59:19 -0800

    Make Client know about all vitals.

--- src/main/java/software/visionary/vitalizr/Client.java
+++ src/main/java/software/visionary/vitalizr/Client.java
@@ -15,7 +15,17 @@ enum Client {
     ADD_FAT(13342),
     LIST_FAT(13343),
     ADD_WATER(13344),
-    LIST_WATER(13345);
+    LIST_WATER(13345),
+    ADD_SUGAR(13346),
+    LIST_SUGAR(13347),
+    ADD_BP(13348),
+    LIST_BP(13349),
+    ADD_TEMP(13350),
+    LIST_TEMP(13351),
+    ADD_O2(13350),
+    LIST_O2(13351),
+    ADD_PULSE(13350),
+    LIST_PULSE(13351);
 
     private int port;
 
@@ -28,20 +38,30 @@ enum Client {
         final String toDispatch = String.format("%s_%s", deque.pop(), deque.pop());
         final Client client = Client.valueOf(toDispatch.toUpperCase());
         client.execute(createRequest(client, deque));
-
     }
 
     private static String createRequest(final Client client, final Deque<String> deque) {
         switch(client) {
-            case ADD_WEIGHT: return String.format("%s&%s&%s\u0004", deque.pop(), deque.pop(), deque.pop());
+            case ADD_WEIGHT:
+            case ADD_TEMP:
+                return String.format("%s&%s&%s\u0004", deque.pop(), deque.pop(), deque.pop());
             case LIST_WEIGHT:
             case LIST_BMI:
             case LIST_FAT:
             case LIST_WATER:
+            case LIST_SUGAR:
+            case LIST_BP:
+            case LIST_TEMP:
+            case LIST_O2:
+            case LIST_PULSE:
                 return String.format("%s\u0004", deque.pop());
             case ADD_BMI:
             case ADD_FAT:
             case ADD_WATER:
+            case ADD_SUGAR:
+            case ADD_BP:
+            case ADD_O2:
+            case ADD_PULSE:
                 return String.format("%s&%s\u0004", deque.pop(), deque.pop());
             default: throw new UnsupportedOperationException("We do not support " + client.name());
         }
